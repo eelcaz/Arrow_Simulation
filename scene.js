0bd51c925d61aa99ff8552fef6fbd45d2a4b1b3a
jared-weinblatt <53191130+jared-weinblatt@users.noreply.github.com>
2021-04-25 21:06:22 -0400

    Render Multiple Objects, rotate and launch arrows
    change the program so that it iterates through an object array when rendering, drawing each object. These objects are added to the array when loaded. Arrows can be rotated to extend using the directional keys, and launched at a set velocity using the space bar. This causes a new arrow to be loaded.

--- scene.js
+++ scene.js
@@ -4,6 +4,7 @@ var canvas;
 var gl;
 var arrow_texture_shader;
 
+var objectArray = [];
 var numVerticesInAllArrowFaces;
 var arrow_texture;
 var arrow_indices;
@@ -20,7 +21,20 @@ var iBuffer;
 var vNormBuffer;
 var vNorm;
 
+var rotateMatrixLoc;
+var translateMatrixLoc;
+var modelViewMatrixLoc;
+var projectionMatrixLoc;
+
+
+let count = 0;
+
 function loadedArrow(data, _callback) {
+    loadArrow(data);
+    _callback();
+}
+
+function loadArrow(data){
     arrow_object = loadOBJFromBuffer(data);
     console.log(arrow_object);
     arrow_indices = arrow_object.i_verts;
@@ -28,7 +42,26 @@ function loadedArrow(data, _callback) {
     numVerticesInAllArrowFaces = arrow_indices.length;
     arrow_normals = getOrderedNormalsFromObj(arrow_object);
     arrow_texture_coords = getOrderedTextureCoordsFromObj(arrow_object);
-    _callback();
+
+    iBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, iBuffer);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(arrow_indices), gl.STATIC_DRAW);
+
+    vBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(arrow_vertices), gl.STATIC_DRAW);
+
+    vTexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vTexBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(flatten(arrow_texture_coords)), gl.STATIC_DRAW);
+
+    vNormBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vNormBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(flatten(arrow_normals)), gl.STATIC_DRAW);
+
+    objectArray.push({iBuffer, vBuffer, vTexBuffer, vNormBuffer, 
+                      numVerticesInAllObjFaces : numVerticesInAllArrowFaces, translateX : 0.0, translateY : 0.0, translateZ : 0.0,
+                      rotateY : 266.0, rotateZ : 2.0, velocityX : 0.0, velocityY : 0.0, velocityZ : 0.0, isMoving : false});
 }
 
 // Properly orders the normals from the OBJ file.
@@ -65,8 +98,6 @@ function getOrderedTextureCoordsFromObj(obj_object) {
 	return texCoordsOrderedWithVertices;
 }
 
-
-
 // Configures initial texture setup.
 function configureTexture(image) {
 	var texture = gl.createTexture();
@@ -83,11 +114,54 @@ function configureTexture(image) {
 	return texture;
 }
 
+window.addEventListener("keydown", function(){
+    let obj = objectArray[objectArray.length-1];
+	switch(event.keyCode) {
+		case 38:  // up arrow key
+            if (obj.rotateZ < 180.0) {
+                obj.rotateZ = Math.min((obj.rotateZ + 2.0), 50.0);
+            }
+            else {
+                obj.rotateZ = ((obj.rotateZ + 2.0) % 360.0);
+            }
+            console.log(obj.rotateZ);
+			break;
+		case 40:  // down arrow key
+            if (obj.rotateZ > 180){
+                obj.rotateZ = Math.max((obj.rotateZ - 2.0), 340.0);
+            }
+            else {
+                obj.rotateZ = ((((obj.rotateZ - 2.0) % 360.0) + 360.0) % 360.0);
+            }
+            console.log(obj.rotateZ);
+			break;
+		case 37: // left arrow key
+            obj.rotateY = Math.max(obj.rotateY - 2.0, 240.0);
+            console.log(obj.rotateY);
+			break;
+		case 39: // right arrow key
+            obj.rotateY = Math.min(obj.rotateY + 2.0, 300.0);
+            console.log(obj.rotateY);
+			break;
+		case 32: // spacebar
+            let velocity = 0.05;
+            obj.velocityX = velocity * Math.cos(Math.PI * obj.rotateY / 180.0);
+            obj.velocityY = velocity * Math.sin(Math.PI * obj.rotateZ / 180.0);
+            obj.velocityZ = velocity * Math.cos(Math.PI * obj.rotateZ / 180.0);
+            
+            obj.isMoving = true;
+            loadOBJFromPath("Arrow2.obj", loadArrow);
+			break;
+		}
+        console.log(`velocityX: ${obj.velocityX}`);
+        console.log(`velocityY: ${obj.velocityY}`);
+        console.log(`velocityZ: ${obj.velocityZ}`);
+
+}, true);
+
 function setupAfterDataLoad() {
     gl.enable(gl.DEPTH_TEST);
 
-    setupArrowShaderBuffers();
-    gl.useProgram(arrow_texture_shader);
     var image = document.getElementById("arrow_texture");
     gl.activeTexture(gl.TEXTURE0);
     arrow_texture = configureTexture(image);
@@ -106,59 +180,70 @@ window.onload = function init() {
     
     gl.clearColor(1.0, 1.0, 1.0, 1.0);
 
-    loadOBJFromPath("Arrow2.obj", loadedArrow, setupAfterDataLoad);
-}
-
-function setupArrowShaderBuffers() {
     arrow_texture_shader = initShaders(gl, "arrow-vertex-shader", "arrow-fragment-shader");
     gl.useProgram(arrow_texture_shader);
-
-    iBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, iBuffer);
-    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(arrow_indices), gl.STATIC_DRAW);
-
-    vBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(arrow_vertices), gl.STATIC_DRAW);
-
-    vTexBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, vTexBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(flatten(arrow_texture_coords)), gl.STATIC_DRAW);
-
-    vNormBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, vNormBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(flatten(arrow_normals)), gl.STATIC_DRAW);
 
     vPosition = gl.getAttribLocation(arrow_texture_shader, "vPosition");
     vTexCoord = gl.getAttribLocation(arrow_texture_shader, "vTexCoord");
     vNorm = gl.getAttribLocation(arrow_texture_shader, "vNorm");
 
+    rotateMatrixLoc = gl.getUniformLocation( arrow_texture_shader, "rotateMatrix" );
+    translateMatrixLoc = gl.getUniformLocation( arrow_texture_shader, "translateMatrix" );
+    modelViewMatrixLoc = gl.getUniformLocation( arrow_texture_shader, "modelViewMatrix" );
+    projectionMatrixLoc = gl.getUniformLocation( arrow_texture_shader, "projectionMatrix" );
+    loadOBJFromPath("Arrow2.obj", loadedArrow, setupAfterDataLoad);
 }
 
-function renderArrow() {
-    gl.useProgram(arrow_texture_shader);
+function renderObj(obj) {
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, obj.iBuffer);
 
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, iBuffer);
-
-    gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
+    gl.bindBuffer(gl.ARRAY_BUFFER, obj.vBuffer);
     gl.vertexAttribPointer(vPosition, 3, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(vPosition);
 
-    gl.bindBuffer(gl.ARRAY_BUFFER, vTexBuffer);
+    gl.bindBuffer(gl.ARRAY_BUFFER, obj.vTexBuffer);
     gl.vertexAttribPointer(vTexCoord, 2, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(vTexCoord);
 
-    gl.bindBuffer(gl.ARRAY_BUFFER, vNormBuffer);
+    gl.bindBuffer(gl.ARRAY_BUFFER, obj.vNormBuffer);
     gl.vertexAttribPointer(vNorm, 3, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(vNorm);
 
-    gl.drawElements(gl.TRIANGLES, numVerticesInAllArrowFaces, gl.UNSIGNED_SHORT, 0);
+    let rotateMatrix = mult(rotate(obj.rotateY, 0.0, 1.0, 0.0), rotate(obj.rotateZ, 0.0, 0.0, 1.0));
+    gl.uniformMatrix4fv( rotateMatrixLoc, false, flatten(rotateMatrix) );
+
+    if (obj.isMoving){
+        obj.translateX += obj.velocityZ;
+        obj.translateY += obj.velocityY;
+        obj.translateZ -= obj.velocityX;
+
+        let translateMatrix = translate(obj.translateX, obj.translateY, obj.translateZ);
+        gl.uniformMatrix4fv( translateMatrixLoc, false, flatten(translateMatrix) );
+    }
+    else {
+        //gl.uniformMatrix4fv( translateMatrixLoc, false, flatten(mat4()));
+        gl.uniformMatrix4fv( translateMatrixLoc, false, flatten(translate(0.0,0.0,0.0)));
+    }
+
+    let modelViewMatrix = mat4();
+    let projectionMatrix = mat4();
+    //let modelViewMatrix = lookAt( eye, at, up );
+    //var scale = 1;
+	//let projectionMatrix = ortho(-1.0*scale, 1.0*scale, -1.0*scale, 1.0*scale, -1.0*scale, 1.0*scale);
+    gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(modelViewMatrix));
+    gl.uniformMatrix4fv(projectionMatrixLoc, false, flatten(projectionMatrix));
+
+    gl.drawElements(gl.TRIANGLES, obj.numVerticesInAllObjFaces, gl.UNSIGNED_SHORT, 0);
 }
 
 function render() {
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-    renderArrow();
+    if (count % 500 == 0) console.log(objectArray);
+    count++;
+    for (let obj of objectArray){
+        renderObj(obj);
+    }
 
     requestAnimationFrame(render);
 }
